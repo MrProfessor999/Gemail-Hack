7fd00a5a7bcc21386ba39fd36e77bdf1b98978ce
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-13 11:22:26 +0530

    Update gemailhack.py

--- gemailhack.py
+++ gemailhack.py
@@ -35,3 +35,39 @@ def main():
    print ('     |     {__)              ') 
    print ('           ()   V.1.0        ')
 
+#!/usr/bin/python
+'''create by Ha3MrX'''
+
+import smtplib
+from os import system
+
+def main():
+   print '================================================='
+   print '               create by Ha3MrX                  '
+   print '================================================='
+   print '               ++++++++++++++++++++              '
+   print '\n                                               '
+   print '  _,.                                            '
+   print '                                                 '
+   print '                                                 '
+   print '           HA3MrX                                '
+   print '       _,.                   '
+   print '     ,` -.)                  '
+   print '    ( _/-\\-._               '
+   print '   /,|`--._,-^|            , '
+   print '   \_| |`-._/||          , | '
+   print '     |  `-, / |         /  / '
+   print '     |     || |        /  /  '
+   print '      `r-._||/   __   /  /   '
+   print '  __,-<_     )`-/  `./  /    '
+   print '  \   `---    \   / /  /     '
+   print '     |           |./  /      '
+   print '     /           //  /       '
+   print ' \_/  \         |/  /        '
+   print '  |    |   _,^- /  /         '
+   print '  |    , ``  (\/  /_         '
+   print '   \,.->._    \X-=/^         '
+   print '   (  /   `-._//^`           '
+   print '    `Y-.____(__}             '
+   print '     |     {__)              ' 
+   print '           ()   V.1.0        '
