08aa4183c0b7a022ae6259d5b79e629c91b85fab
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-13 11:44:00 +0530

    Add files via upload

---
+++ gemailhack.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+'''create by NAVIN'''
+
+import smtplib
+from os import system
+
+def main():
+   print ("create by NAVIN")
+   print ("Hi Jomon enthoke ind")
+   print ("Set alle pover🔥🔥🔥")
